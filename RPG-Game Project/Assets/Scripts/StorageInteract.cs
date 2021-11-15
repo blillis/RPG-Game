2c1f00b000672b261a7281d40c8e7e1f41e74108
blillis <76559682+blillis@users.noreply.github.com>
2021-11-15 19:42:20 +1300

    Chest prefab and NPC

--- RPG-Game Project/Assets/Scripts/StorageInteract.cs
+++ RPG-Game Project/Assets/Scripts/StorageInteract.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class StorageInteract : Interactable
 {
-    [SerializeField] GameObject closed;
-    [SerializeField] GameObject open;
+    [SerializeField] GameObject closedStorage;
+    [SerializeField] GameObject openStorage;
     [SerializeField] bool opened;
     // Start is called before the first frame update
     public override void Interact(Character character)
     {
-        
+        if(opened == false)
+        {
+            opened = true;
+            closedStorage.SetActive(false);
+            openStorage.SetActive(true);
+        }
     }
 }
