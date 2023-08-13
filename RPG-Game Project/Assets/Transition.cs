9a010af24b8ff1fa84365eb8164f5020160e133f
blillis <76559682+blillis@users.noreply.github.com>
2023-08-13 13:19:29 +1200

    Camera and smooth transitions
    Added camera borders and smoother transitions between scenes

--- RPG-Game Project/Assets/Transition.cs
+++ RPG-Game Project/Assets/Transition.cs
@@ -23,12 +23,10 @@ public class Transition : MonoBehaviour
 
     internal void InitiateTransition(Transform toTransition)
     {
-        Cinemachine.CinemachineBrain currentCamera = Camera.main.GetComponent<Cinemachine.CinemachineBrain>();
-        
-
         switch (transitionType) 
         {
             case TransitionType.Warp:
+                Cinemachine.CinemachineBrain currentCamera = Camera.main.GetComponent<Cinemachine.CinemachineBrain>();
                 currentCamera.ActiveVirtualCamera.OnTargetObjectWarped(
                     toTransition,
                     destination.position - toTransition.position
@@ -40,11 +38,8 @@ public class Transition : MonoBehaviour
                 );
                 break;
             case TransitionType.Scene:
-                currentCamera.ActiveVirtualCamera.OnTargetObjectWarped(
-                    toTransition,
-                    targetPosition - toTransition.position
-                );
-                GameSceneManager.instance.SwitchScene(sceneNameToTransition, targetPosition);
+                
+                GameSceneManager.instance.InitSwitchScene(sceneNameToTransition, targetPosition);
                 break;
         }
 
