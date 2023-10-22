080402dec4f9019aa91cf3a275483bd0db48720e
blillis <76559682+blillis@users.noreply.github.com>
2023-10-22 16:32:22 +1300

    audio
    Added audio and music managers

--- RPG-Game Project/Assets/Scripts/StorageInteract.cs
+++ RPG-Game Project/Assets/Scripts/StorageInteract.cs
@@ -7,6 +7,8 @@ public class StorageInteract : Interactable
     [SerializeField] GameObject closedStorage;
     [SerializeField] GameObject openStorage;
     [SerializeField] bool opened;
+    [SerializeField] AudioClip onOpenAudio;
+    [SerializeField] AudioClip onCloseAudio;
     // Start is called before the first frame update
     public override void Interact(Character character)
     {
@@ -15,6 +17,15 @@ public class StorageInteract : Interactable
             opened = true;
             closedStorage.SetActive(false);
             openStorage.SetActive(true);
+
+            AudioManager.instance.Play(onOpenAudio);
+        } else
+        {
+            opened = false;
+            closedStorage.SetActive(true);
+            openStorage.SetActive(false);
+
+            AudioManager.instance.Play(onCloseAudio);
         }
     }
 }
