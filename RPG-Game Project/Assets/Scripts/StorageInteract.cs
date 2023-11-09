bb6db525050126ec7e9386d13f899c80b69ea11c
blillis <76559682+blillis@users.noreply.github.com>
2023-11-09 20:58:04 +1300

    chest storage
    finished storage for chest

--- RPG-Game Project/Assets/Scripts/StorageInteract.cs
+++ RPG-Game Project/Assets/Scripts/StorageInteract.cs
@@ -9,32 +9,42 @@ public class StorageInteract : Interactable
     [SerializeField] bool opened;
     [SerializeField] AudioClip onOpenAudio;
     [SerializeField] AudioClip onCloseAudio;
-    //[SerializeField] Character c;
+    [SerializeField] ItemContainer itemContainer;
     // Start is called before the first frame update
     public override void Interact(Character character)
     {
         
         if (opened == false)
         {
-            opened = true;
-            closedStorage.SetActive(false);
-            openStorage.SetActive(true);
-
-            AudioManager.instance.Play(onOpenAudio);
-
-            character.GetComponent<StorageInteractController>().Open();
+            Open(character);
         }
         else
         {
-            opened = false;
-            closedStorage.SetActive(true);
-            openStorage.SetActive(false);
-
-            AudioManager.instance.Play(onCloseAudio);
-
-            character.GetComponent<StorageInteractController>().Close();
+            Close(character);
         }
         
         
+    }
+
+    public void Open(Character character)
+    {
+        opened = true;
+        closedStorage.SetActive(false);
+        openStorage.SetActive(true);
+
+        AudioManager.instance.Play(onOpenAudio);
+
+        character.GetComponent<StorageInteractController>().Open(itemContainer, transform);
+    }
+
+    public void Close(Character character)
+    {
+        opened = false;
+        closedStorage.SetActive(true);
+        openStorage.SetActive(false);
+
+        AudioManager.instance.Play(onCloseAudio);
+
+        character.GetComponent<StorageInteractController>().Close();
     }
 }
--- RPG-Game Project/Assets/StorageInteractController.cs
+++ RPG-Game Project/Assets/StorageInteractController.cs
@@ -5,15 +5,41 @@ using UnityEngine;
 public class StorageInteractController : MonoBehaviour
 {
     ItemContainer targetItemContainer;
+    InventoryController inventoryController;
     [SerializeField] StoragePanel itemStoragePanel;
+    Transform openedChest;
+    [SerializeField] float maxDistance = 0.8f;
 
-    public void Open()
+    private void Awake()
     {
+        inventoryController = GetComponent<InventoryController>();
+    }
+
+    private void Update()
+    {
+        if (openedChest != null)
+        {
+            float distance = Vector2.Distance(openedChest.position, transform.position);
+            if (distance > maxDistance)
+            {
+                openedChest.GetComponent<StorageInteract>().Close(GetComponent<Character>());
+            }
+        }
+    }
+
+    public void Open(ItemContainer itemContainer, Transform _openedChest)
+    {
+        targetItemContainer = itemContainer;
+        itemStoragePanel.inventory = targetItemContainer;
+        inventoryController.Open();
         itemStoragePanel.gameObject.SetActive(true);
+        openedChest = _openedChest;
     }
 
     public void Close()
     {
+        inventoryController.Close();
         itemStoragePanel.gameObject.SetActive(false);
+        openedChest = null;
     }
 }
