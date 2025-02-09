d43d6e1da60543d975005508e429970cec5177ef
blillis <bayleylillis@yahoo.co.nz>
2025-02-09 15:41:04 +1300

    perissitant spawning
    half way through (15.08) Persistant Spawned obj

--- RPG-Game Project/Assets/ObjectSpawner.cs
+++ RPG-Game Project/Assets/ObjectSpawner.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
+[RequireComponent(typeof(TimeAgent))]
 public class ObjectSpawner : MonoBehaviour
 {
     [SerializeField] float spawnArea_height = 1f;
@@ -15,6 +15,8 @@ public class ObjectSpawner : MonoBehaviour
 
     [SerializeField] bool oneTime = false;
 
+    List<SpawnedObject> spawnedObjects;
+
     private void Start()
     {
         length = spawn.Length;
@@ -23,13 +25,20 @@ public class ObjectSpawner : MonoBehaviour
         {
             TimeAgent timeAgent = GetComponent<TimeAgent>();
             timeAgent.onTimeTick += Spawn;
+            spawnedObjects = new List<SpawnedObject>();
         }
         else
         {
             Spawn();
             Destroy(gameObject);
         }
-        
+
+    }
+
+    public void SpawnedObjectDestroyed(SpawnedObject spawnedObject)
+    {
+        spanwedObjects.Remove(spawnedObject);
+
     }
 
     void Spawn()
@@ -38,14 +47,65 @@ public class ObjectSpawner : MonoBehaviour
 
         for (int i = 0; i < spawnCount; i++)
         {
-            GameObject go = Instantiate(spawn[Random.Range(0, length)]);
+            int id = Random.Range(0, length);
+            GameObject go = Instantiate(spawn[id]);
             Transform t = go.transform;
+
+            if (oneTime == false)
+            {
+                t.SetParent(transform);
+                SpawnedObject spawnedObject = t.GetComponent<SpawnedObject>();
+                spawnedObjects.Add(spawnedObject);
+                spawnedObject.objId = id;
+            }
 
             Vector3 position = transform.position;
             position.x += UnityEngine.Random.Range(-spawnArea_width, spawnArea_width);
             position.y += UnityEngine.Random.Range(-spawnArea_height, spawnArea_height);
 
             t.position = position;
+        }
+    }
+
+    public class ToSave
+    {
+
+        public List<SpawnedObject.SaveSpawnedObjectData> spawnedObjectDatas;
+
+        public ToSave()
+        {
+            spawnedObjectDatas = new List<SpawnedObject.SaveSpawnedObjectData>();
+        }
+    }
+
+    string Read()
+    {
+        ToSave toSave = new ToSave();
+
+        for (int i = 0; i < spawnedObjects.Count; i++)
+        {
+            toSave.spawnedObjectDatas.Add(
+                new SpawnedObject.SaveSpawnedObjectData(
+                    spawnedObjects[i].objId,
+                    spawnedObjects[i].transform.position
+                    )
+                );
+        }
+        return JsonUtility.ToJson(toSave);
+    }
+
+    public void Load(string json)
+    {
+        if(json == "" || json == "{}" || json == null) { return; }
+
+        ToSave toLoad = JsonUtility.FromJson<ToSave>(json);
+
+        for (int i = 0; i < toLoad.spawnedObjectDatas.Count; i++)
+        {
+            SpawnedObject.SaveSpawnedObjectData data = toLoad.spawnedObjectDatas[i];
+            GameObject go = Instantiate(spawn[data.objectId]);
+            go.transform.position = data.worldPosition;
+            go.GetComponent<SpawnedObject>().objId = data.objectId;
         }
     }
 
